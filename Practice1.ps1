bea29cb5b1bc3ff1dc8b8ad45eafeb3bb7a4483b
Saeid <pournejati@gmail.com>
2020-12-17 23:20:59 -0600

    First practice file.

---
+++ Practice1.ps1
@@ -0,0 +1,16 @@
+Clear-Host
+
+Get-Service |
+Where-Object Status -EQ 'Stopped' |
+Select-Object DisplayName, Status
+
+# $ServiceList = Get-Service |
+# Where-Object Status -EQ 'Stopped' |
+# Select-Object DisplayName, Status
+
+# $ServiceList
+
+# $ServiceList | Out-File .\Services.txt
+# Notepad .\MyFile.txt
+# $ServiceList | Export-Csv .\MyFile.csv
+# Get-Content .\Services.csv | more
