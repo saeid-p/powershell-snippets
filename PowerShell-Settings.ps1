78bb211dd083e17cd67e3b2c2cf61757c1307a85
Saeid Pournejati <pournejati@gmail.com>
2020-12-22 12:26:47 -0600

    Updated the default settings with more explanation.

--- PowerShell-Settings.ps1
+++ PowerShell-Settings.ps1
@@ -1,5 +1,5 @@
 # https://www.hanselman.com/blog/how-to-make-a-pretty-prompt-in-windows-terminal-with-powerline-nerd-fonts-cascadia-code-wsl-and-ohmyposh
-# Font: "Cascadia Code PL"
+# Font: "Cascadia Code PL": https://github.com/microsoft/cascadia-code/releases
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
 Install-Module -Name PSReadLine
