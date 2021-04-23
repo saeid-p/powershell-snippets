35c757a464cc85e7e702d31b75817f1de20bd89d
Sayid <6471104+sayid-coder@users.noreply.github.com>
2021-04-23 13:40:12 -0500

    Add .NET Core templates to check runtimes/SDKs

--- DotNet-Utils.ps1
+++ DotNet-Utils.ps1
@@ -3,3 +3,14 @@ Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -Recurse |
   Get-ItemProperty -Name version -EA 0 |
   Where { $_.PSChildName -Match '^(?!S)\p{L}'} | 
   Select PSChildName, version
+
+# .NET Core: https://docs.microsoft.com/en-us/dotnet/core/install/how-to-detect-installed-versions
+
+# Get .NET Core version.
+dotnet --version
+
+# Get .NET Core installed sdks.
+dotnet --list-sdks
+
+# Get .NET Core runtimes.
+dotnet --list-runtimes
